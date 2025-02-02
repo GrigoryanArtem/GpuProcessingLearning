ebd079b335df08fe8374bb4e7df896e98b48df2b
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-02-02 23:37:30 +0200

    Add shaders and light management to Scene class

--- GPL.OpenGLApp/Base/Scene.cs
+++ GPL.OpenGLApp/Base/Scene.cs
@@ -1,10 +1,30 @@
 ﻿using GPL.OpenGLApp.Geometry;
+using GPL.OpenGLApp.Light;
 
 namespace GPL.OpenGLApp.Base;
 
 public class Scene : IDisposable
 {
+    private readonly DefaultShader _defaultShader;
+    private readonly ShaderProgram _lightShader;
+    private readonly ShaderProgram _screenShader;
+
+    public Scene()
+    {
+        Console.Error.WriteLine("Shaders loading...");
+
+        _defaultShader = new();
+        _lightShader = new ShaderProgram("shaders/light.vert", "shaders/light.frag");
+        _screenShader = new ShaderProgram("shaders/fb.vert", "shaders/fb.frag");
+
+        Console.Error.WriteLine("Shaders loaded");
+    }
+
     public List<IGeometryObject> geometries = [];
+
+    public List<PointLight> _pointLights = [];
+    public List<DirectionLight> _dirLights = [];
+    public List<SpotLight> _spotLights= [];
 
     public void Add(IEnumerable<IGeometryObject> geometryObjects)
     {
@@ -19,6 +39,36 @@ public class Scene : IDisposable
     public void Delete(IGeometryObject geometryObject)
     {
         geometries.Remove(geometryObject);
+    }
+
+    public void Add(DirectionLight light)
+    {
+        _dirLights.Add(light);
+    }
+
+    public void Delete(DirectionLight light)
+    {
+        _dirLights.Remove(light);
+    }
+
+    public void Add(SpotLight light)
+    {
+        _spotLights.Add(light);
+    }
+
+    public void Delete(SpotLight light)
+    {
+        _spotLights.Remove(light);
+    }
+
+    public void Add(PointLight light)
+    {
+        _pointLights.Add(light);
+    }
+
+    public void Delete(PointLight light)
+    {
+        _pointLights.Remove(light);
     }
 
     public void Clear()
